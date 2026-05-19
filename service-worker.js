04f4f047449ba4448ab3c07b7be1ce53c6b7b228
ErsinBinal <ersinbinal@gmail.com>
2026-05-19 17:26:33 +0300

    kul hatti new stage

--- service-worker.js
+++ service-worker.js
@@ -3,7 +3,7 @@
  * Offline destek ve cache yonetimi
  */
 
-const CACHE_NAME = 'convivium-v42';
+const CACHE_NAME = 'convivium-v43';
 const OFFLINE_URL = '/offline.html';
 
 // Cache'lenecek dosyalar
